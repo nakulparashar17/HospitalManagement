7c5688ca57e8f49ddb65dfe896f34b29f80fa5ec
faisal <faisal@LAPTOP-GCQM8HGP>
2020-05-05 13:20:58 +0530

    first commit

---
+++ HospitalManagement2/src/test/java/com/techie/HospitalManagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
